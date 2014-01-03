82578cdd83f027aa3a3017db618c6a07e9cc94a5
Ohm <chimmee.s@gmail.com>
2014-01-03 12:42:59 +0700

    update signup responsive form

--- application/views/form/signup.php
+++ application/views/form/signup.php
@@ -1,68 +1,60 @@
-<center>
-	<div class="signup-box">
-		<h1>Sign Up</h1>
-<?php echo form_open_multipart('form/signup'); ?>
-
-<div class="left">
+<div class="row">
+		<center>
+		<div class="large-8 columns signup-box">
+			<h1>Sign Up</h1>
+			<?php echo form_open_multipart('form/signup'); ?>
+				
 			<div class="row">
-				<div class="small-4 columns">
-					<label for="right-label" class="right inline">Email*</label>
-				</div>
-				<div class="small-8 columns">
-					<input type="text" id="right-label" name="email"placeholder="Enter your email">
-				</div>
-				<?php echo form_error('email','<font color="error">');?>
-			</div>
-
-			<div class="row">
-				<div class="small-4 columns">
-					<label for="right-label" class="right inline">Password*</label>
-				</div>
-				<div class="small-8 columns">
-					<input type="password" id="right-label" name="password"placeholder="Enter your password">
-				</div>
-				<?php echo form_error('password','<font color="error">');?>
-			</div>
-
-			<div class="row">
-				<div class="small-4 columns">
-					<label for="right-label" class="right inline">Name*</label>
-				</div>
-				<div class="small-8 columns">
-					<input type="text" id="right-label" name="name"placeholder="Enter your name">
-				</div>
-				<?php echo form_error('name','<font color="error">');?>
-			</div>
-		</div>
-		<div class="right">
-			<div class="row">
-				<div class="small-3 columns">
-					<label for="right-label" class="right inline">Phone</label>
-				</div>
-				<div class="small-9 columns">
-					<input type="text" id="right-label" name="phone"
+				<div class="large-6 columns">
+					<div class="small-4 columns">
+						<label for="right-label" class="right inline">Email*</label>
+					</div>
+					<div class="large-8 columns">
+						<input type="text" id="right-label" name="email"placeholder="Enter your email">
+					</div>
+					<?php echo form_error('email','<font color="error">');?>
+					<div class="large-4 columns">
+						<label for="right-label" class="right inline">Password*</label>
+					</div>
+					<div class="large-8 columns">
+						<input type="password" id="right-label" name="password"placeholder="Enter your password">
+					</div>
+					<?php echo form_error('password','<font color="error">');?>
+					<div class="large-4 columns">
+						<label for="right-label" class="right inline">Name*</label>
+					</div>
+					<div class="large-8 columns">
+						<input type="text" id="right-label" name="name"placeholder="Enter your name">
+					</div>
+					<?php echo form_error('name','<font color="error">');?>
+					<div class="large-4 columns">
+						<label for="right-label" class="right inline">Phone</label>
+					</div>
+					<div class="large-8 columns">
+						<input type="text" id="right-label" name="phone"
 						placeholder="Enter your phone number">
+					</div>
+					
 				</div>
-			</div>
-
-			<div class="row">
-				<div class="small-3 columns">
-					<label for="right-label" class="right inline">Avatar</label>
+				<div class="large-6 columns">
+					<div class="large-4 columns">
+						<label for="right-label" class="right inline">Avatar</label>
+					</div>
+					<div class="large-8 columns">
+						<input type="file" id="right-label" name="avatar">
+					</div>
+					<div class="large-12 columns">
+						<input type="submit" value="Sign Up" class="button signin-btn success" />
+					</div>
+					<div class="large-12 columns">
+						<a href="signin" class="button signin-btn">Sign In</a>
+					</div>
 				</div>
-				<div class="small-9 columns">
-					<input type="file" id="right-label" name="avatar">
-				</div>
-			</div>
-
-			<div>
-				<input type="submit" value="Sign Up" class="button signin-btn" />
-			</div>
-			
-		</div>
-		
-		</form>
-	</div>
 	
-</center>
+		</form>
+		</div>
+		</center>
+</div>
+
 </body>
 </html>
