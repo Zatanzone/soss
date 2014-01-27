045684c970427222b090f318ff4beb8a5b469259
Ohm <chimmee.s@gmail.com>
2014-01-27 19:07:35 +0700

    Knowledge - test commit

--- application/views/knowledge/knowledge.php
+++ application/views/knowledge/knowledge.php
@@ -1,1 +1,8 @@
-<center><h2>Comming Soon...</h2></center>
+<div class="row">
+	<div class="large-12 columns">
+		<h4>Knowledge ISO/IEC 29110 29110-5-1-2</h4>
+		<div class="panel">
+		<p  style=" margin: 12px auto 6px auto; font-family: Helvetica,Arial,Sans-serif; font-style: normal; font-variant: normal; font-weight: normal; font-size: 14px; line-height: normal; font-size-adjust: none; font-stretch: normal; -x-system-font: none; display: block;">   <a title="View ISO Knowledge on Scribd" href="http://www.scribd.com/doc/202470827/ISO-Knowledge"  style="text-decoration: underline;" ></p><iframe class="scribd_iframe_embed" src="//www.scribd.com/embeds/202470827/content?start_page=1&view_mode=scroll&access_key=key-1beo6vm30dy4f2cufzd5&show_recommendations=true" data-auto-height="false" data-aspect-ratio="0.706896551724138" scrolling="no" id="doc_3232" width="100%" height="600" frameborder="0"></iframe>
+		</div>	
+	</div>
+</div>
